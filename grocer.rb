664a6372b5db99d67f5da5ceaf6349ad5a186a45
Dan Kagan <dankagan7@gmail.com>
2019-05-09 13:34:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,7 +42,7 @@ def apply_clearance(cart)
   cart
 end
 
-def checkout(cart: [], coupons: [])
+def checkout(cart, coupons)
   total = 0
   consoldiated_cart = consolidate_cart(cart)
   cart_with_coupons = apply_coupons(consoldiated_cart, coupons)
